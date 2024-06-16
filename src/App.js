f003c1d370d5b71b2806a163efa6eb322a0bf6b1
Amulyac13 <amulyachoudha@gmail.com>
2024-06-16 23:28:11 +0530

    upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route,Routes } from "react-router-dom";
+import {Box} from '@mui/material'; // a box is a div with some shading and colors.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import './App.css';
+const App=()=>{
+    return(
+        <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto"> 
+        {/* m mean margin auto it is a property of material ui and sx is support responsive design in extra large devices (xl means extra large) */}
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
+    )
 }
 
-export default App;
+export default App
